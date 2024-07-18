e5b31a4cf86e6a24e552d9545f332e511ee39a32
Sri Charan Thoutam <thoutamsricharan@gmail.com>
2024-07-18 19:35:27 +0530

    update app.py

--- app.py
+++ app.py
@@ -52,7 +52,6 @@ def index():
     else:
         return render_template('index.html')
 
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
